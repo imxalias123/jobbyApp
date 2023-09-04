1a17cc7696606d011720e3304a21836337afc424
sailaxmi <sailaxmikomirineni@gmail.com>
2023-09-04 16:19:34 +0000

    solution

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,28 +1,38 @@
 import './index.css'
+import {Component} from 'react'
 
-const LoginForm = () => (
-  <div className="login-container">
-    <div className="login-card">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
-        alt="website logo"
-        className="logo"
-      />
-      <form className="form-container" onSubmit={onSubmitForm()}>
-        <label className="label" htmlFor="username">
-          USERNAME
-        </label>
-        <input className="input" placeholder="Username" id="username" />
-        <label className="label" htmlFor="password">
-          PASSWORD
-        </label>
-        <input className="input" placeholder="Password" id="password" />
-        <button type="submit" className="login-btn">
-          Login
-        </button>
-      </form>
-    </div>
-  </div>
-)
+class LoginForm extends Component {
+  state = {
+    onSubmitError: false,
+  }
+
+  render() {
+    const onSubmitError = this.state
+    return (
+      <div className="login-container">
+        <div className="login-card">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+            className="logo"
+          />
+          <form className="form-container" onSubmit={this.onSubmitForm()}>
+            <label className="label" htmlFor="username">
+              USERNAME
+            </label>
+            <input className="input" placeholder="Username" id="username" />
+            <label className="label" htmlFor="password">
+              PASSWORD
+            </label>
+            <input className="input" placeholder="Password" id="password" />
+            <button type="submit" className="login-btn">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default LoginForm
