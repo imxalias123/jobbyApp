09b18d2686687c3121b0447b0231dbd07bd630eb
sailaxmi <sailaxmikomirineni@gmail.com>
2023-09-06 11:34:40 +0000

    solution

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,43 @@
 import './index.css'
-import {Link} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import {Link, withRouter} from 'react-router-dom'
 import {AiFillHome} from 'react-icons/ai'
 import {BsBriefcase} from 'react-icons/bs'
 import {FiLogOut} from 'react-icons/fi'
 
-const Header = () => (
-  <div className="header-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
-      alt="website logo"
-      className="header-logo"
-    />
-    <div className="icons-container">
-      <AiFillHome className="home-icon" size={25} />
-      <BsBriefcase className="case-icon" size={25} />
-      <FiLogOut className="logout-icon" size={25} />
-    </div>
+const Header = props => {
+  const {history} = props
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
 
-    <div className="link-container">
-      <Link className="link" to="/">
-        <p>Home</p>
-      </Link>
-      <Link className="link" to="/jobs">
-        <p>Jobs</p>
-      </Link>
-    </div>
-    <button type="button" className="logout-btn">
-      Logout
-    </button>
-  </div>
-)
+  return (
+    <div className="header-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+        alt="website logo"
+        className="header-logo"
+      />
+      <div className="icons-container">
+        <AiFillHome className="home-icon" size={25} />
+        <BsBriefcase className="case-icon" size={25} />
+        <FiLogOut className="logout-icon" size={25} />
+      </div>
 
-export default Header
+      <div className="link-container">
+        <Link className="link" to="/">
+          <p>Home</p>
+        </Link>
+        <Link className="link" to="/jobs">
+          <p>Jobs</p>
+        </Link>
+      </div>
+      <button type="button" onClick={onClickLogout} className="logout-btn">
+        Logout
+      </button>
+    </div>
+  )
+}
+
+export default withRouter(Header)
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,32 @@
 import './index.css'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
 import Header from '../Header'
 
-const Home = () => (
-  <div className="home">
-    <Header />
-    <div className="home-container">
-      <div className="text-container">
-        <h1 className="home-heading">Find The Job That Fits Your Life</h1>
-        <p className="home-para">
-          Millions of people are searching for jobs, salary information, company
-          reviews. Find the job that fits your abilities and potential.
-        </p>
-        <button className="find-jobs" type="button">
-          Find Jobs
-        </button>
+const Home = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return (
+    <div className="home">
+      <Header />
+      <div className="home-container">
+        <div className="text-container">
+          <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+          <p className="home-para">
+            Millions of people are searching for jobs, salary information,
+            company reviews. Find the job that fits your abilities and
+            potential.
+          </p>
+          <button className="find-jobs" type="button">
+            Find Jobs
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Home
