b0a0920ab2fc604ea40bc8a37a4f9fde4b371839
sailaxmi <sailaxmikomirineni@gmail.com>
2023-09-11 08:10:28 +0000

    solution

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -19,6 +19,44 @@ const apiStatusConstants = {
   inProgress: 'IN_PROGRESS',
 }
 
+const employmentTypesList = [
+  {
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
+  },
+  {
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
+  },
+  {
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
+  },
+  {
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
+  },
+]
+
+const salaryRangesList = [
+  {
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
+  },
+  {
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
+  },
+  {
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
+  },
+  {
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
+  },
+]
+
 class Jobs extends Component {
   state = {
     profileData: [],
@@ -161,13 +199,20 @@ class Jobs extends Component {
         <Header />
         <div className="jobs-container">
           <div>
-            <input placeholder="Search" />
-            <div className="profile-container">
+            <div className="side-container">
               {this.renderProfileStatus()}
               <hr className="hr-line" />
+              <h1>Type of Employment</h1>
+              {this.renderCheckboxInput}
+              <hr className="hr-line" />
+              <h1>Salary Range</h1>
+              {this.renderRadioInput}
             </div>
           </div>
-          {this.renderJobStatus()}
+          <div>
+            <input placeholder="Search" />
+            {this.renderJobStatus()}
+          </div>
         </div>
       </>
     )
